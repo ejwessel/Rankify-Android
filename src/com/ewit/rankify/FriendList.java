cc77909bfc0697a6527797a5acf597a141ba6d8d
Ethan Wessel <ejwessel@gmail.com>
2014-07-23 13:03:57 -0700

    fixed an issue with one of the classes being extended improperly from another

--- src/com/ewit/rankify/FriendList.java
+++ src/com/ewit/rankify/FriendList.java
@@ -7,13 +7,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class FriendList extends Activity {
+public class FriendList extends CustomActivity {
 
 	ListView friendList;
 	ArrayList<String> nameList = new ArrayList<String>();
@@ -77,14 +78,11 @@ public class FriendList extends Activity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	public boolean onOptionsItemSelected(MenuItem menuItem) {
+
+		super.onOptionsItemSelected(menuItem);
+		startActivity(new Intent(FriendList.this, CalculateActivity.class));
+		return true;
 	}
+
 }
