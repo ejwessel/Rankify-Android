a64a2b1f4e93615b019492074400bc85a0a41da0
Ethan Wessel <ejwessel@gmail.com>
2014-07-26 15:08:46 -0700

    lazy loading view for ranked friends. also added a new arrow icon

--- src/com/ewit/rankify/FriendDataAdapter.java
+++ src/com/ewit/rankify/FriendDataAdapter.java
@@ -1,9 +1,15 @@
 package com.ewit.rankify;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
-import android.content.ClipData.Item;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +17,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.fedorvlasov.lazylist.ImageLoader;
+
 public class FriendDataAdapter extends ArrayAdapter<String> {
 	// declaring our ArrayList of items
 	private ArrayList<String> objects;
+	private ImageLoader imageLoader;
 
 	/*
 	 * here we must override the constructor for ArrayAdapter the only variable
@@ -23,6 +32,7 @@ public class FriendDataAdapter extends ArrayAdapter<String> {
 	public FriendDataAdapter(Context context, int textViewResourceId, ArrayList<String> objects) {
 		super(context, textViewResourceId, objects);
 		this.objects = objects;
+		imageLoader = new ImageLoader(context);
 	}
 
 	/*
@@ -50,8 +60,16 @@ public class FriendDataAdapter extends ArrayAdapter<String> {
 		 * Therefore, i refers to the current Item object.
 		 */
 		String individual = objects.get(position);
+		JSONObject userData = null;
+		try {
+			JSONObject data = new JSONObject(individual);
+			String userDataString = data.getString("User");
+			userData = new JSONObject(userDataString);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-		if (individual != null) {
+		if (userData != null) {
 
 			// This is how you obtain a reference to the TextViews.
 			// These TextViews are created in the XML files we defined.
@@ -62,26 +80,71 @@ public class FriendDataAdapter extends ArrayAdapter<String> {
 
 			// check to see if each individual textview is null.
 			// if not, assign some text!
-			if (userImage != null) {
-				userImage.setBackground(null);
+			if (userImage == null) {
+				userImage.setBackground(null);				
 			} else {
-				//need to pull from a url
+				// show The Image
+				try {
+					String profilePictureSmall = userData.getString("profilePictureSmall");
+//					new ImageDownloader(userImage).execute(profilePictureSmall);
+					imageLoader.DisplayImage(profilePictureSmall, userImage);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
 			}
-			if (userName != null) {
+			if (userName == null) {
 				userName.setText("NO NAME");
 			} else {
-				userName.setText("TEST NAME");
+				try {
+					userName.setText(userData.getString("name"));
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
 			}
-			if (userRank != null) {
+			if (userRank == null) {
 				userRank.setText("NO RANK");
 			} else {
-				userRank.setText("TEST RANK");
+				try {
+					int totalLikes = userData.getInt("totalLikes");
+					int totalComments = userData.getInt("totalComments");
+					userRank.setText(String.valueOf(totalLikes + totalComments));
+
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 
 		// the view must be returned to our activity
 		return v;
 
+	}
+
+	class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+		ImageView bmImage;
+
+		public ImageDownloader(ImageView bmImage) {
+			this.bmImage = bmImage;
+		}
+
+		protected Bitmap doInBackground(String... urls) {
+			String url = urls[0];
+			Bitmap mIcon = null;
+			try {
+				InputStream in = new java.net.URL(url).openStream();
+				mIcon = BitmapFactory.decodeStream(in);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return mIcon;
+		}
+
+		protected void onPostExecute(Bitmap result) {
+			
+			System.out.println("downloaded image" + result);
+			
+			bmImage.setImageBitmap(result);
+		}
 	}
 
 }
