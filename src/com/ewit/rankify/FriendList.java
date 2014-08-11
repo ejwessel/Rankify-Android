33c00faae93799ffa51b37cab1d0166e918b852c
Ethan Wessel <ejwessel@gmail.com>
2014-08-10 19:26:25 -0700

    changed the link to the page that will take the user to iOS or android downloads

--- src/com/ewit/rankify/FriendList.java
+++ src/com/ewit/rankify/FriendList.java
@@ -143,7 +143,7 @@ public class FriendList extends CustomActivity implements SearchView.OnQueryText
 			Bundle postParams = new Bundle();
 			postParams.putString("name", "Rankify");
 			postParams.putString("caption", "Top 10 Friends:");
-			postParams.putString("link", "http://e-wit.co.uk/rankifyapp/index.php");
+			postParams.putString("link", "http://e-wit.co.uk/rankifyapp/index.html");
 			postParams.putString("picture", null);
 			postParams.putString("description", topFriends);
 
